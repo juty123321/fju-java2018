745b719990ef60d6926388e14adc4aa7b11cff16
user <user@es302n30>
2018-10-02 11:02:55 +0800

    exam

---
+++ exam/src/com/fju/Hello/Hello.java
@@ -0,0 +1,19 @@
+package com.fju.Hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Java is too diffcult to me");
+		int age = 20;
+		float height = 172.6f;
+		float weight = 53.4f;
+		System.out.println("my name is Rita");
+		System.out.println(age);
+		System.out.println(height);
+		System.out.println(weight);
+		
+
+	}
+
+}
